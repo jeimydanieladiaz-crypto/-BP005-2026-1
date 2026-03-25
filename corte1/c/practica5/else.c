b9b6522dee3e4619bc14880f873c08fa1ea4be93
jeimydanieladiaz-crypto <jeimydanieladiaz@gmail.com>
2026-03-24 20:35:15 -0500

    Update else.c

--- corte1/c/practica5/else.c
+++ corte1/c/practica5/else.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+ 
+int main () {
 
+  int time;
+
+printf("Ingrese la hora en formato militar: ");
+    scanf("%d", &time);
+
+  if (time < 18) {
+    printf("Buen día.");
+
+
+
+} else {
+  printf("Buenas noches.");
+
+}
+
+return 0;
+
+}  
